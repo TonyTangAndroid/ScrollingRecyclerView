ede666a3c3452bb1aa86bf4dd957fb001e72a982
Zhilu Tang <tonythompsoncmu@gmail.com>
2023-04-15 18:24:46 -0700

    Enforce Google Java format.

--- app/src/main/java/com/stylingandroid/smoothscrolling/MainActivity.java
+++ app/src/main/java/com/stylingandroid/smoothscrolling/MainActivity.java
@@ -9,36 +9,37 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    private RecyclerView recyclerView;
+  private RecyclerView recyclerView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        recyclerView.setAdapter(LargeAdapter.newInstance(this));
-        int duration = getResources().getInteger(R.integer.scroll_duration);
-        recyclerView.setLayoutManager(new ScrollingLinearLayoutManager(this, LinearLayoutManager.VERTICAL, false, duration));
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+    recyclerView.setAdapter(LargeAdapter.newInstance(this));
+    int duration = getResources().getInteger(R.integer.scroll_duration);
+    recyclerView.setLayoutManager(
+        new ScrollingLinearLayoutManager(this, LinearLayoutManager.VERTICAL, false, duration));
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+
+    if (id == R.id.action_top) {
+      recyclerView.smoothScrollToPosition(0);
+      return true;
+    } else if (id == R.id.action_bottom) {
+      recyclerView.smoothScrollToPosition(recyclerView.getAdapter().getItemCount());
+      return true;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_top) {
-            recyclerView.smoothScrollToPosition(0);
-            return true;
-        } else if (id == R.id.action_bottom) {
-            recyclerView.smoothScrollToPosition(recyclerView.getAdapter().getItemCount());
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 }
