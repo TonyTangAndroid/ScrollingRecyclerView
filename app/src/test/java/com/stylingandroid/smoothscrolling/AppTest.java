16f42f5271fea13b9bf42a77c73bc5be588ec1ad
Zhilu Tang <tonythompsoncmu@gmail.com>
2023-04-15 19:54:17 -0700

    Add AppTest

--- app/src/test/java/com/stylingandroid/smoothscrolling/AppTest.java
+++ app/src/test/java/com/stylingandroid/smoothscrolling/AppTest.java
@@ -1,9 +1,20 @@
 package com.stylingandroid.smoothscrolling;
 
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.google.common.truth.Truth;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
 
+@Config(application = App.class)
+@RunWith(AndroidJUnit4.class)
 public class AppTest {
 
   @Test
-  public void test123() {}
+  public void test123() {
+    Context context = ApplicationProvider.getApplicationContext();
+    Truth.assertThat(context).isInstanceOf(App.class);
+  }
 }
