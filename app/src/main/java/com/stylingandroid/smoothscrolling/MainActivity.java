8a8ce9ce535dec2a628ee76707dd1fd065f596f2
Tony Tang <tonytang@uber.com>
2023-09-01 14:39:26 -0700

    Upgrade fast scroll related code

--- app/src/main/java/com/stylingandroid/smoothscrolling/MainActivity.java
+++ app/src/main/java/com/stylingandroid/smoothscrolling/MainActivity.java
@@ -2,46 +2,44 @@ package com.stylingandroid.smoothscrolling;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends Activity {
 
-    private RecyclerView recyclerView;
+  private RecyclerView recyclerView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recyclerview);
-        recyclerView.setAdapter(LargeAdapter.newInstance(this));
-        int duration = getResources().getInteger(R.integer.scroll_duration);
-        recyclerView.setLayoutManager(new ScrollingLinearLayoutManager(this, LinearLayoutManager.VERTICAL, false, duration));
-        FastScroller fastScroller = findViewById(R.id.fastscroller);
-        fastScroller.setRecyclerView(recyclerView);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+    recyclerView.setAdapter(LargeAdapter.newInstance(this));
+    int duration = getResources().getInteger(R.integer.scroll_duration);
+    recyclerView.setLayoutManager(
+        new ScrollingLinearLayoutManager(this, LinearLayoutManager.VERTICAL, false, duration));
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+
+    if (id == R.id.action_top) {
+      recyclerView.smoothScrollToPosition(0);
+      return true;
+    } else if (id == R.id.action_bottom) {
+      recyclerView.smoothScrollToPosition(recyclerView.getAdapter().getItemCount());
+      return true;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_top) {
-            recyclerView.smoothScrollToPosition(0);
-            return true;
-        } else if (id == R.id.action_bottom) {
-            RecyclerView.Adapter adapter = recyclerView.getAdapter();
-            assert adapter != null;
-            recyclerView.smoothScrollToPosition(adapter.getItemCount());
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 }
