82413fb15b68dc93379e50e65af219eb5d7110b3
Tony Tang <tonytang@uber.com>
2023-09-01 15:04:57 -0700

    Confirm that Extra FastScrollListener will mess up the default dx and dy value in  OnScrollListener:onScrolled callback

--- app/src/main/java/com/stylingandroid/smoothscrolling/MainActivity.java
+++ app/src/main/java/com/stylingandroid/smoothscrolling/MainActivity.java
@@ -2,13 +2,18 @@ package com.stylingandroid.smoothscrolling;
 
 import android.app.Activity;
 import android.os.Bundle;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends Activity {
 
+    private AtomicInteger accumulatedX = new AtomicInteger(0);
+    private AtomicInteger accumulatedY = new AtomicInteger(0);
     private RecyclerView recyclerView;
 
     @Override
@@ -21,6 +26,14 @@ public class MainActivity extends Activity {
         recyclerView.setLayoutManager(new ScrollingLinearLayoutManager(this, LinearLayoutManager.VERTICAL, false, duration));
         FastScroller fastScroller = (FastScroller) findViewById(R.id.fastscroller);
         fastScroller.setRecyclerView(recyclerView);
+        recyclerView.addOnScrollListener(new OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+              int x=  accumulatedX.accumulateAndGet(dx,Integer::sum);
+                int y = accumulatedY.accumulateAndGet(dy,Integer::sum);
+                System.out.printf("accumulated x=%s,y=%s \n",x,y);
+            }
+        });
     }
 
     @Override
